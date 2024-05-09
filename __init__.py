eb05b429780ce6426c60869986702c1e53d99a1f
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-05-09 10:12:04 -0500

    requirements

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
+import requests
 
+def get_post_ids() -> list[int]:
+    content = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').json()
 
-def get_hacker_news():
     return 'Hacker News'
+
+def get_comments_from_posts(url: str) -> list[str]:
+
+    return 'Hacker News'
+
+def get_sentiment(content: str) -> float:
+
+    return 0.5
+
+
+if __name__ == '__main__':
+    print(get_post_ids())p
