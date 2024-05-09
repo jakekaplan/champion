3a8ba263500877549fa909313a00fd95603cc8fa
jake@prefect.io <jake@prefect.io>
2024-05-09 11:02:44 -0500

    merge stuff

--- run.py
+++ run.py
@@ -5,17 +5,70 @@ import pendulum
 from uuid import uuid4
 from google.oauth2 import service_account
 from google.cloud import storage
-
+import requests
+import os
+from typing import List, Optional, Any
+from pydantic import BaseModel, HttpUrl, Field
+import openai
+from marvin import fn
 
 BUCKET_NAME = "jake-adam-kevin"
-
 image = modal.Image.debian_slim(python_version="3.11").pip_install_from_requirements("requirements.txt")
-
 app = modal.App("scrape-data-app", image=image)
 
-@app.function(secrets=[])
-def f():
-    print(os.environ["OPEN_AI_SECRET"])
+
+class Item(BaseModel):
+    by: Optional[str] = None
+    descendants: Optional[int] = None
+    id: Optional[int] = None
+    kids: List[int] = Field(default_factory = list)
+    score: Optional[int] = None
+    text: Optional[Any] = None
+    time: Optional[int] = None
+    title: Optional[str] = None
+    type: Optional[str] = None
+    url: Optional[HttpUrl] = None
+
+
+class CommentSentiment(BaseModel):
+    comment_id: int
+    post_id: int
+    sentiment: float
+
+
+def get_post_ids() -> list[int]:
+    return requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').json()
+
+
+def get_detail(id: int) -> Item:
+    '''
+    All HackerNews posts are represented hierarchically using a common structure.
+    '''
+    content = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty').json()
+    return Item(**content)
+
+
+def sentiment(text: str) -> float:
+    """
+    Returns a sentiment score for `text`
+    between -1 (negative) and 1 (positive).
+    """
+
+
+def get_comment_sentiments(post_id: int) -> List[CommentSentiment]:
+    result: List[CommentSentiment] = []
+    for kid in get_detail(post_id).kids:
+        sentiment_score = fn(sentiment)(get_detail(kid).text)
+        result.append(CommentSentiment(comment_id=kid, post_id=post_id, sentiment=sentiment_score))
+    return result
+
+
+def get_top_hackernews_comments_sentiments() -> List[CommentSentiment]:
+    post_ids = get_post_ids()
+    result: List[CommentSentiment] = []
+    for post_id in post_ids:
+        result.extend(get_comment_sentiments(post_id))
+    return result
 
 
 def get_gcs_client() -> storage.Client:
@@ -29,17 +82,20 @@ def get_gcs_client() -> storage.Client:
     schedule=modal.Period(minutes=1),
     secrets=[
         modal.Secret.from_name("jake-adam-kevin-creds"),
-        modal.Secret.from_name("open-ai-secret"),
+        modal.Secret.from_name("open-api-secret"),
     ])
 def upload_data():
+    comments = get_top_hackernews_comments_sentiments()
+
     client = get_gcs_client()
     bucket = client.bucket("jake-adam-kevin")
     file_name = f"{pendulum.now().format('YYYY-MM-DD-HH-mm-ss')}-{uuid4()}"
     blob = bucket.blob(file_name)
 
-    data = """{"post_id": 1, "comment_id": 2, "sentiment": 0.5}"""
+    data = '\n'.join([comment.model_dump_json() for comment in comments])
     blob.upload_from_string(data)
 
+    print("Uploaded data to GCS")
 
 @app.local_entrypoint()
 def main():
